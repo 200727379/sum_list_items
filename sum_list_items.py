fa798bb646511858dcd8be4e32053ccef7804965
200727379 <rampedilesley@gmail.com>
2021-04-28 14:03:24 -0700

    initial commit

---
+++ sum_list_items.py
@@ -0,0 +1,16 @@
+#Author: Mothapo Rampedi Lesley
+#Email: rampedilesley@gmail.com
+#Date: 28 April 2021
+
+
+"""
+Write a Python program to sum all the items in a list.
+"""
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] #list with 10 items
+
+total = 0
+
+for x in my_list:
+    total += x
+
+print("The sum of all the items in a list is ", total)
